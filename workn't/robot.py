0af350a8cb1961bec3dbc42946ee1bbb53893021
nikilokser <32608425+nikilokser@users.noreply.github.com>
2020-01-07 01:27:46 +0700

    Create robot.py

---
+++ workn't/robot.py
@@ -0,0 +1,169 @@
+import socket
+from time import sleep
+
+
+class Connection:
+    sock = socket.socket()
+    connected = False
+
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.connect()
+
+    def connect(self):
+        self.sock = socket.socket()
+        self.sock.connect((self.host, self.port))
+        self.connected = True
+
+    def disconnect(self):
+        self.sock.close()
+        self.connected = False
+
+    def reconnect(self):
+        self.disconnect()
+        self.connect()
+
+    def request(self, req):
+        while True:
+            try:
+                self.sock.send((req + '\n').encode())
+                sleep(0.01)
+                data = self.sock.recv(1024)
+                if b'\xf0' in data:
+                    return None
+                elif b'\xf1' in data:
+                    return data.replace(b'\xf1', b'').replace(b'\r\n', b'').decode()
+                else:
+                    raise BaseException(b'Request error: ' + data)
+            except (ConnectionResetError, ConnectionAbortedError):
+                self.reconnect()
+
+
+class Platform(Connection):
+    def __init__(self, host):
+        self.host = host
+        super().__init__(self.host, 11099)
+
+    def go(self, speed=None, dir=None):
+        if speed is not None:
+            self.request('robot:motors:R.WheelF:velset:' + str(speed))
+            self.request('robot:motors:R.WheelB:velset:' + str(speed))
+            self.request('robot:motors:L.WheelF:velset:' + str(speed))
+            self.request('robot:motors:L.WheelB:velset:' + str(speed))
+        if dir is not None:
+            self.request('robot:motors:R.DriveWheelF:posset:' + str(dir))
+            self.request('robot:motors:L.DriveWheelF:posset:' + str(dir))
+
+    def go_time(self, t, speed=None, dir=None):
+        self.go(speed, dir)
+        sleep(t)
+        self.stop()
+
+    def stop(self):
+        self.go(speed=0, dir=0)
+
+
+class Body(Connection):
+    def __init__(self, host):
+        self.host = host
+        super().__init__(self.host, 10099)
+        self.motors = self.get_all_motors_data()
+
+    def get_sensor_data(self, sensor):
+        head = self.request('robot:sensors:{}:list'.format(sensor))
+        head = head.split(';')
+
+        data = self.request('robot:sensors:{}'.format(sensor))
+        data = data.split(';')
+
+        if len(head) != len(data):
+            raise BaseException('Length is not equal')
+
+        return dict(zip(head, data))
+
+    def get_all_motors_data(self):
+        motors = self.request('robot:motors:list').split(';')
+        motors.pop(-1)
+
+        M = {}
+        for e in motors:
+            M[e] = round(float(self.request('robot:motors:{}:posget'.format(e)).replace(';', '')))
+
+        return M
+
+    def index(self):
+        for e in self.motors:
+            self.request('robot:motors:{}:posset:{}'.format(e, self.motors[e]))
+
+    def fist(self):
+        self.request('robot:motors:L.Finger.Index:posset:-45')
+        self.request('robot:motors:L.Finger.Middle:posset:-45')
+        self.request('robot:motors:L.Finger.Ring:posset:-45')
+        self.request('robot:motors:L.Finger.Little:posset:-45')
+        self.request('robot:motors:L.Finger.Thumb:posset:-55')
+
+    def antifist(self):
+        self.request('robot:motors:L.Finger.Index:posset:0')
+        self.request('robot:motors:L.Finger.Middle:posset:0')
+        self.request('robot:motors:L.Finger.Ring:posset:0')
+        self.request('robot:motors:L.Finger.Little:posset:0')
+        self.request('robot:motors:L.Finger.Thumb:posset:0')
+
+    def open_box(self):
+        self.request('robot:motors:L.ShoulderF:posset:10')
+        self.request('robot:motors:TorsoR:posset:50')
+        self.request('robot:motors:TorsoF:posset:-20')
+        self.request('robot:motors:L.WristF:posset:-20')
+        self.request('robot:motors:L.Elbow:posset:-50')
+        self.request('robot:motors:L.ShoulderS:posset:80')
+        sleep(1)
+        self.request('robot:motors:TorsoR:posset:-80')
+        sleep(1)
+        self.request('robot:motors:L.ShoulderS:posset:70')
+        self.request('robot:motors:L.WristS:posset:-20')
+        sleep(1)
+        self.fist()
+        sleep(1)
+        self.request('robot:motors:L.ShoulderS:posset:100')
+        self.request('robot:motors:TorsoR:posset:-90')
+        sleep(1)
+        self.antifist()
+        self.request('robot:motors:L.WristS:posset:20')
+        self.request('robot:motors:L.Elbow:posset:0')
+        sleep(1)
+        self.request('robot:motors:R.ShoulderF:posset:-150')
+        sleep(1)
+        self.request('robot:motors:TorsoR:posset:40')
+        sleep(1)
+        self.request('robot:motors:R.ShoulderF:posset:-40')
+        sleep(1)
+        self.request('robot:motors:TorsoR:posset:-60')
+        self.request('robot:motors:R.ShoulderF:posset:-60')
+        sleep(1)
+
+
+class Scene:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    def __init__(self, host):
+        self.host = host
+
+    def reset(self):
+        self.sock.sendto(b'scene:reset\n', (self.host, 10000))
+        sleep(3)
+
+
+class Robot:
+    def __init__(self, host, platform=False):
+        self.host = host
+
+        self.body = Body(self.host)
+
+        if platform:
+            self.platform = Platform(self.host)
+        else:
+            self.request = self.body.request
+
+        self.scene = Scene(self.host)
+        self.reset = self.scene.reset
